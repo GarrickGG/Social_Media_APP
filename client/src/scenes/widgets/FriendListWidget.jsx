b8481d8740e49c2768113d20a52191171d7b4864
Guoyi Zhang <122499836+GarrickGG@users.noreply.github.com>
2023-06-18 14:10:32 -0400

    Update FriendListWidget.jsx

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://http://social-media-backend.fly.dev/${userId}/friends`,
+      `https://social-media-backend.fly.dev/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
