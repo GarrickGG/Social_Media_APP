1693293f93380c2f373374605c96e52cc1a0aeec
Guoyi Zhang <122499836+GarrickGG@users.noreply.github.com>
2023-06-18 14:07:50 -0400

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://social-media-backend.fly.dev/${_id}/${friendId}`,
+      `https://social-media-backend.fly.dev/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
