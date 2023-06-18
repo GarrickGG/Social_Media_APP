0f99ae6bfd3438e3f510e1698f09bf44c2d01081
Guoyi Zhang <122499836+GarrickGG@users.noreply.github.com>
2023-06-18 14:09:07 -0400

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://social-media-backend.fly.dev/auth/register",
+      "https://social-media-backend.fly.dev/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://social-media-backend.fly.dev/auth/login", {
+    const loggedInResponse = await fetch("https://social-media-backend.fly.dev/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
