56bb8bb2eef8c7be3d691b8ae02fbe949bee0041
Guoyi Zhang <122499836+GarrickGG@users.noreply.github.com>
2023-06-18 14:12:22 -0400

    Update UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -22,7 +22,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const response = await fetch(`http://social-media-backend.fly.dev/users/${userId}`, {
+    const response = await fetch(`https://social-media-backend.fly.dev/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
