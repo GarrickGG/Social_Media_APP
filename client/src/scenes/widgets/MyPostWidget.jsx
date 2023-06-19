d2ead323d7e4c1e40e2350c17a15c22f32c5abc0
Guoyi Zhang <122499836+GarrickGG@users.noreply.github.com>
2023-06-18 21:21:03 -0400

    Update MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`https://social-media-backend.fly.dev/posts`, {
+    const response = await fetch(`https://social-media-backend.fly.dev:6001/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
