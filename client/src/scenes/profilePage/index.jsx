d6446c6e65f2fdc41240ad6bac5264fd2dd9bf47
Guoyi Zhang <122499836+GarrickGG@users.noreply.github.com>
2023-06-18 21:19:46 -0400

    Update index.jsx

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-    const response = await fetch(`https://social-media-backend.fly.dev/${userId}`, {
+    const response = await fetch(`https://social-media-backend.fly.dev:6001/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
